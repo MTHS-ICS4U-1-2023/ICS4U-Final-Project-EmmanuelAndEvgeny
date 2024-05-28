4553451f9b7d00d48bf9b93f086393df86935fa9
Evgeny Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2024-05-28 14:06:43 +0000

    ok

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,5 +1,5 @@
 /**
-  * This is the mainMenu.ts code that runs
+  * This is the MainMenu.ts code that runs
   * By: Evgeny Vovk
   * Version: 1.0
   * Since: 2024-05-21
@@ -20,9 +20,9 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.background = this.add.image(512, 384, 'background');
+        this.background = this.add.image(512, 384, './public/assets/images/bg.png');
 
-        this.logo = this.add.image(512, 300, 'logo');
+        this.logo = this.add.image(512, 300, './public/assets/images/logo.png');
 
         this.title = this.add.text(512, 460, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
