c753eedd064aa8d1d488f2cf87fad0b2b18f43cb
Evgeny Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2024-05-29 13:09:05 +0000

    dd

--- dist/bundle.js
+++ dist/bundle.js
@@ -192,8 +192,8 @@ class MainMenu extends phaser_minExports.Scene {
         });
     }
     create() {
-        this.background = this.add.image(512, 384, './public/assets/images/bg.png');
-        this.logo = this.add.image(512, 300, './public/assets/images/logo.png');
+        this.background = this.add.image(512, 384, 'background');
+        this.logo = this.add.image(512, 300, 'logo');
         this.title = this.add.text(512, 460, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -20,9 +20,9 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.background = this.add.image(512, 384, './public/assets/images/bg.png');
+        this.background = this.add.image(512, 384, 'background');
 
-        this.logo = this.add.image(512, 300, './public/assets/images/logo.png');
+        this.logo = this.add.image(512, 300, 'logo');
 
         this.title = this.add.text(512, 460, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
